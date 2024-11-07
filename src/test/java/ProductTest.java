c49d4aa342c0771f0c5222615f191be976fc6f5d
uxoa <palomita@gmail.com>
2024-11-07 21:41:25 +0100

    Feat: Starting Java Shopping Kata

--- src/test/java/ProductTest.java
+++ src/test/java/ProductTest.java
@@ -8,15 +8,16 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class ProductTest {
     @Test
     @DisplayName("Crear un producto con nombre y precio")
-    public void test_crearUnProductoConNombreYPrecio(){
-        //GIVEN
-        Product product = new Product("producto01", 1000.0);
-        //WHEN
-        assertEquals("producto01", product.getName());
-        assertEquals(1000.0, product.getPrice());
+    public void test_crearUnProductoConIdNombreYPrecio(){
+
+        Product product = new Product(1, "producto01", 47.0);
+        assertEquals(1, product.getProductId());
+        assertEquals("producto01", product.getProductName());
+        assertEquals(47.0,product.getProductPrice());
 
 
-        //THEN
+
+
     }
 
 }
