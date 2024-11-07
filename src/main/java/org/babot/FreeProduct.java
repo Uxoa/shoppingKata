c02cdf54e22bc7028c07f56ac1e3724fe7a9e585
uxoa <palomita@gmail.com>
2024-11-07 22:00:03 +0100

    Feat: tdd Java Shopping Kata update

--- src/main/java/org/babot/FreeProduct.java
+++ src/main/java/org/babot/FreeProduct.java
@@ -1,4 +1,12 @@
 package org.babot;
 
 public class FreeProduct {
+
+
+    public FreeProduct(String perejil, double freeProduct) {
+    }
+
+    public Object getProductPrice() {
+        return 0.0;
+    }
 }
--- src/test/java/ProductTest.java
+++ src/test/java/ProductTest.java
@@ -1,3 +1,4 @@
+import org.babot.FreeProduct;
 import org.babot.Product;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -7,8 +8,14 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ProductTest {
     @Test
+    void elPrecioDeLosProductosGratuitosEsSiempreCero() {
+        FreeProduct freeProduct = new FreeProduct("perejil", 0.0);
+        assertEquals(0.0, freeProduct.getProductPrice());
+
+    }
+    @Test
     @DisplayName("Crear un producto con nombre y precio")
-    public void test_crearUnProductoConIdNombreYPrecio(){
+    public void testCrearUnProductoConIdNombreYPrecio(){
 
         Product product = new Product(1, "producto01", 47.0);
         assertEquals(1, product.getProductId());
