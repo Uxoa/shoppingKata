26a3ac2f1776cef7f5509ed4be54317faa241868
uxoa <palomita@gmail.com>
2024-11-16 18:08:26 +0100

    first commit

---
+++ src/main/java/org/example/FreeProduct.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class FreeProduct extends Product {
+    
+    
+    public FreeProduct(String name, double price) {
+        super(name, price);
+    }
+    
+    
+    public void getprice() {
+    
+    }
+}
---
+++ src/test/java/FreeProductTest.java
@@ -0,0 +1,15 @@
+import org.example.FreeProduct;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class FreeProductTest {
+    @Test
+    @DisplayName("elPrecioDeLosProductosGratuitosEsSiempreCero")
+    void elPrecioDeLosProductosGratuitosEsSiempreCero() {
+        FreeProduct freeProduct = new FreeProduct("perejil", 0.0);
+        freeProduct.getprice();
+        assertEquals(0.0, freeProduct.getPrice());
+    }
+}
